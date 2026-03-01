ed6527df26ff2b51682e7fdcb280e8ce045a5ba7
Sebastian Nowicki <sebnow@gmail.com>
2026-03-01 22:34:48 +0100

    feat(nix): add package output to flake
    Build with ReleaseSafe, run tests during check phase.
    Installs binary, man page, and shell completions.

--- flake.nix
+++ flake.nix
@@ -11,6 +11,38 @@
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       perSystem = { pkgs, ... }: {
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "md";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.zig_0_15 ];
+
+            dontConfigure = true;
+            dontFixup = true;
+
+            buildPhase = ''
+              runHook preBuild
+              zig build \
+                --global-cache-dir "$TMPDIR/zig-cache" \
+                --prefix "$out" \
+                -Doptimize=ReleaseSafe
+              runHook postBuild
+            '';
+
+            installPhase = "true"; # zig build --prefix already installs
+
+            doCheck = true;
+            checkPhase = ''
+              runHook preCheck
+              zig build test \
+                --global-cache-dir "$TMPDIR/zig-cache"
+              runHook postCheck
+            '';
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.zig_0_15
